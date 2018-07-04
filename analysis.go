43d802e4b77006716ba21f9db1ef5a2a697fd779
shawpo <2533649281@qq.com>
2018-07-04 19:24:06 +0800

    AddFileEnWords

--- analysis.go
+++ analysis.go
@@ -4,11 +4,13 @@ import (
 	"bufio"
 	"os"
 	"strings"
+	"io/ioutil"
+	"regexp"
 )
 
-func GetWordsMap(path string) (wordsMap map[string] int , err error){
+type WordsMap map[string] int
 
-	wordsMap = make(map[string]int)
+func LoadDictionary(path string, wordsMap *WordsMap) (err error){
 	file, err := os.Open(path)
 
 	if err != nil {
@@ -27,7 +29,29 @@ func GetWordsMap(path string) (wordsMap map[string] int , err error){
 
 		word :=  strings.Split(string(line), "\t")[0]
 		word = strings.Trim(word, " ")
-		wordsMap[word] = 0
+		word = strings.ToUpper(word)
+		(*wordsMap)[word] = 0
+	}
+	return
+}
+
+func AddFileEnWords(path string, wordsMap *WordsMap) (err error) {
+	file, err := os.Open(path)
+
+	if err != nil {
+		return
+	}
+
+	contents, err := ioutil.ReadAll(file)
+	if err != nil {
+		return
+	}
+	re := regexp.MustCompile(`\b[a-zA-Z]+\b`)
+
+	enWords := re.FindAllString(string(contents), -1)
+	for _, enWord := range enWords{
+		enWord = strings.ToUpper(enWord)
+		(*wordsMap)[enWord] = 0
 	}
 	return
 }
--- analysis_test.go
+++ analysis_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestGetWordsMap(t *testing.T) {
+func TestLoadDictionary(t *testing.T) {
 	testData := []struct{
 		path string
 		count int
@@ -10,14 +10,16 @@ func TestGetWordsMap(t *testing.T) {
 	} {
 		{
 			"testdata/words.txt",
-			5,
-			[]string{"字符串", "初始化", "数组", "加载", "客户端"},
+			6,
+			[]string{"字符串", "初始化", "数组", "加载", "客户端", "JAVA"},
 		},
 	}
 
 
 	for _, test := range testData {
-		wordsMap, err := GetWordsMap(test.path)
+
+		var wordsMap = make(WordsMap)
+		err := LoadDictionary(test.path, &wordsMap)
 		t.Log(wordsMap)
 
 		if err != nil {
@@ -25,16 +27,51 @@ func TestGetWordsMap(t *testing.T) {
 		}
 
 		if count := len(wordsMap); count != test.count {
-			t.Fatalf("want %d, get %d", count, test.count)
+			t.Fatalf("want %d, get %d", test.count, count)
 		}
 
 		for _, testWord := range test.words{
-			if _, ok := wordsMap[testWord]; !ok {
-				t.Fatalf("want %s, bug don't contains", testWord)
+			if _, ok := (wordsMap)[testWord]; !ok {
+				t.Fatalf("want %s, but don't contains", testWord)
 			}
 
 		}
 	}
-
-
 }
+
+func TestAddFileEnWords(t *testing.T) {
+	testData := []struct{
+		path string
+		count int
+		words []string
+	} {
+		{
+			"testdata/file.txt",
+			7,
+			[]string{"LINUX", "TCP", "IP", "HTTP", "GO", "C", "PYTHON"},
+		},
+	}
+
+	for _, test := range testData {
+
+		var wordsMap = make(WordsMap)
+		err := AddFileEnWords(test.path, &wordsMap)
+		t.Log(wordsMap)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if count := len(wordsMap); count != test.count {
+			t.Fatalf("want %d, get %d", test.count, count)
+		}
+
+		for _, testWord := range test.words{
+			if _, ok := (wordsMap)[testWord]; !ok {
+				t.Fatalf("want %s, but don't contains", testWord)
+			}
+
+		}
+	}
+}
+
